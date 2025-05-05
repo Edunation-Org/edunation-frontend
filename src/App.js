b615ce1ad53087128f66bcf9ee71162b62801085
Basel Ismail Fawaz <134176929+bfawwaz@users.noreply.github.com>
2025-05-05 19:51:42 -0400

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Now
+          shu ya damme 
         </a>
       </header>
     </div>
