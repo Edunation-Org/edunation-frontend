84c7d01a762f07d6c961661bc028368572b26404
Basel Ismail Fawaz <134176929+bfawwaz@users.noreply.github.com>
2025-05-07 16:18:10 -0400

    oops

--- src/global/components/Footer.jsx
+++ src/global/components/Footer.jsx
@@ -17,7 +17,7 @@ const Footer = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("base_url/contact", {
+      const response = await fetch(`${process.env.REACT_APP_BASE_URL}/contact`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
